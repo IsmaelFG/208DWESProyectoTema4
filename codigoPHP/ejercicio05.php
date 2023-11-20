2d704980ec7eb77cc2fb8f064b3326cd26c67221
IsmaelFG <daw2@IS32WX08>
2023-11-20 08:24:35 +0100

    Version_1.1 Actualizacion ejercicios

--- codigoPHP/ejercicio05.php
+++ codigoPHP/ejercicio05.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>codigo04</title>
+        <title>codigo03</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="/208DWESProyectoTema3/webroot/css/style.css">
         <style>
@@ -105,40 +105,54 @@
     <body style="margin-top:70px; margin-bottom: 100px">
         <nav class="navbar navbar-expand-lg bg-primary fixed-top">
             <div class="container">
-                <a class="navbar-brand text-white" href="/index.html">Ejercicio04</a>
+                <a class="navbar-brand text-white" href="/index.html">Ejercicio03</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
             </div>
         </nav>
-
         <?php
+        $dsn = 'mysql:host=192.168.20.19;dbname=DB208DWESProyectoTema4';
+        $username = 'user208DWESProyectoTema4';
+        $password = 'paso';
+        include_once('../core/231018libreriaValidacion.php');
+        define('FECHA_ACTUAL', date('Y-m-d H:i:s'));
+        $entradaOK = true;
         try {
-            $dsn = 'mysql:host=192.168.20.19;dbname=DB208DWESProyectoTema4';
-            $username = 'user208DWESProyectoTema4';
-            $password = 'paso';
-            include_once('../core/231018libreriaValidacion.php');
+            // Conexion con la Base de datos
             $miDB = new PDO($dsn, $username, $password);
-            $miDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Configuramos las excepciones
-            //TRANSACCION
+            // Configuramos las excepciones
+            $miDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // Deshabilitamos el modo autocommit
             $miDB->beginTransaction();
-
             // Consultas SQL de inserción 
-            $consultaInsercion1 = "INSERT INTO T02_Departamento(T02_CodDepartamento,T02_DescDepartamento,T02_VolumenNegocio) VALUES ('DOF', 'Departamento de Ofimatica', 500)";
-            $consultaInsercion2 = "INSERT INTO T02_Departamento(T02_CodDepartamento,T02_DescDepartamento,T02_VolumenNegocio) VALUES ('DID', 'Departamento de I+D', 200)";
-            $consultaInsercion3 = "INSERT INTO T02_Departamento(T02_CodDepartamento,T02_DescDepartamento,T02_VolumenNegocio) VALUES ('DGT', 'Departamento de Gestion', 1000)";
+            $consultaInsercion = "INSERT T02_Departamento(T02_CodDepartamento, T02_DescDepartamento, T02_FechaCreacionDepartamento, T02_VolumenNegocio, T02_FechaBaja) "
+                    . "VALUES (:CodDepartamento, :DescDepartamento, :FechaCreacionDepartamento, :VolumenNegocio, :FechaBaja)";
 
             // Preparamos las consultas
-            $resultadoconsultaInsercion1 = $miDB->prepare($consultaInsercion1);
-            $resultadoconsultaInsercion2 = $miDB->prepare($consultaInsercion2);
-            $resultadoconsultaInsercion3 = $miDB->prepare($consultaInsercion3);
+            $resultadoconsultaInsercion = $miDB->prepare($consultaInsercion);
 
-            // Ejecuto las consultas preparadas y mostramos la tabla en caso 'true' o un mensaje de error en caso de 'false'.
-            // (La función 'execute()' devuelve un valor booleano que indica si la consulta se ejecutó correctamente o no.)
-            if ($resultadoconsultaInsercion1->execute() && $resultadoconsultaInsercion2->execute() && $resultadoconsultaInsercion3->execute()) {
-                $miDB->commit(); // Confirma los cambios y los consolida
+            // ARRAY CON REGISTROS
+            $aDepartamentosNuevos = [
+                ['CodDepartamento' => 'STM', 'DescDepartamento' => 'Departamento de Sistemas', 'FechaCreacionDepartamento' => FECHA_ACTUAL, 'VolumenNegocio' => 550, 'FechaBaja' => null],
+                ['CodDepartamento' => 'FNT', 'DescDepartamento' => 'Departamento de Fuentes', 'FechaCreacionDepartamento' => FECHA_ACTUAL, 'VolumenNegocio' => 100, 'FechaBaja' => null]
+            ];
+
+            foreach ($aDepartamentosNuevos as $departamento) { //Recorremos los registros que vamos a insertar en la tabla
+                $aResgistros = [':CodDepartamento' => $departamento['CodDepartamento'],
+                    ':DescDepartamento' => $departamento['DescDepartamento'],
+                    ':FechaCreacionDepartamento' => $departamento['FechaCreacionDepartamento'],
+                    ':VolumenNegocio' => $departamento['VolumenNegocio'],
+                    ':FechaBaja' => $departamento['FechaBaja']];
+                if (!$resultadoconsultaInsercion->execute($aResgistros)) {
+                    $entradaOK = false;
+                    break;
+                }
+            }
+            if ($entradaOK) {
+                // Confirma los cambios
+                $miDB->commit(); 
                 echo ("<div class='respuestaCorrecta'>Los datos se han insertado correctamente en la tabla Departamento.</div>");
 
                 // Preparamos y ejecutamos la consulta SQL
@@ -146,42 +160,47 @@
                 $resultadoConsultaPreparada = $miDB->prepare($consulta);
                 $resultadoConsultaPreparada->execute();
 
-                // Creamos una tabla en la que mostraremos la tabla de la BD
+                // Tabla para mostrar los resultados
                 echo ("<div class='list-group text-center'>");
                 echo ("<table>
-                                        <thead>
-                                        <tr>
-                                            <th>CodDepartamento</th>
-                                            <th>DescDepartamento</th>
-                                            <th>FechaBaja</th>
-                                            <th>VolumenNegocio</th>
-                                        </tr>
-                                        </thead>");
+                                    <thead>
+                                    <tr>
+                                        <th>Codigo de Departamento</th>
+                                        <th>Descripcion de Departamento</th>
+                                        <th>Fecha de Creacion</th>
+                                        <th>Volumen de Negocio</th>
+                                        <th>Fecha de Baja</th>
+                                    </tr>
+                                    </thead>");
 
-                // Aqui recorremos todos los valores de la tabla, columna por columna, usando fetchObject()
+                //Recorremos todos los valores de la tabla, columna por columna, usando fetchObject() , 
                 echo ("<tbody>");
                 while ($oDepartamento = $resultadoConsultaPreparada->fetchObject()) {
                     echo ("<tr>");
-                    echo "<td>$oDepartamento->T02_CodDepartamento</td>"; //Obtener los códigos.
-                    echo "<td>$oDepartamento->T02_DescDepartamento</td>"; //Obtener las descripciones.
-                    echo "<td>$oDepartamento->T02_FechaCreacionDepartamento</td>"; //Obtener la fecha de creacion
-                    echo "<td>$oDepartamento->T02_VolumenNegocio</td>"; //Obtener el volumen de negocio.
-                    echo "<td>$oDepartamento->T02_FechaBaja</td>"; //Obtener la fecha de baja.
+                    echo ("<td>" . $oDepartamento->T02_CodDepartamento . "</td>");
+                    echo ("<td>" . $oDepartamento->T02_DescDepartamento . "</td>");
+                    echo ("<td>" . $oDepartamento->T02_FechaCreacionDepartamento . "</td>");
+                    echo ("<td>" . $oDepartamento->T02_VolumenNegocio . "</td>");
+                    echo ("<td>" . $oDepartamento->T02_FechaBaja . "</td>");
                     echo ("</tr>");
                 }
 
                 echo ("</tbody>");
             }
         } catch (PDOException $miExcepcionPDO) {
-            $miDB->rollback(); //  Revierte o deshace los cambios
-            $errorExcepcion = $miExcepcionPDO->getCode(); // Almacenamos el código del error de la excepción en la variable '$errorExcepcion'
-            $mensajeExcepcion = $miExcepcionPDO->getMessage(); // Almacenamos el mensaje de la excepción en la variable '$mensajeExcepcion'
+            //Revierte los cambios si algo sale mal
+            // Almacenamos el código del error de la excepción en la variable '$errorExcepcion'
+            // Almacenamos el mensaje de la excepción en la variable '$mensajeExcepcion'
+            $miDB->rollback();
+            $errorExcepcion = $miExcepcionPDO->getCode();
+            $mensajeExcepcion = $miExcepcionPDO->getMessage();
 
-            echo ("<div class='errorException'>Hubo un error al insertar los datos en la tabla Departamento campo duplicado.<br></div>");
-            echo "<span class='errorException'>Error: </span>" . $mensajeExcepcion . "<br>"; // Mostramos el mensaje de la excepción
-            echo "<span class='errorException'>Código del error: </span>" . $errorExcepcion; // Mostramos el código de la excepción
+            echo ("<div class='errorException'>Hubo un error al insertar los datos en la tabla Departamento.<br></div>");
+            echo "<span class='errorException'>Error: </span>" . $mensajeExcepcion . "<br>";
+            echo "<span class='errorException'>Código del error: </span>" . $errorExcepcion;
         } finally {
-            unset($miDB); // Para cerrar la conexión
+            //Cerramos la conexion
+            unset($miDB); 
         }
         ?>
         <footer class="bg-primary text-light py-4 fixed-bottom">
